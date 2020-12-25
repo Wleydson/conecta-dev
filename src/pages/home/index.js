2dee2afd436021187e17c54ed632f631c346f075
Wleydson <wleydsonredes@gmail.com>
2020-12-25 19:31:21 -0300

    iniciando a estrutura da aplicação

---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+
+import Header from './components/Header'
+import Main from './components/Main'
+
+function Home(){
+    return(
+        <div>
+            <Header />
+            <Main />
+        </div>
+    )
+}
+
+export default Home
