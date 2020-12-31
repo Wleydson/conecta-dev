a187b9181e5476ed1fc446d532aa77a5270336d0
Wleydson Lemos <wleydsonredes@gmail.com>
2020-12-31 11:35:41 -0300

    customizando o component navBar

--- src/pages/home/components/NavBar.js
+++ src/pages/home/components/NavBar.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+import { Paper, Button, ListSubheader, ListItem, ListItemText } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root:{
+       padding: theme.spacing(2),
+       width: 275,
+       marginRight: theme.spacing(2),
+    },
+    button:{
+        width: '100%'
+    }
+}))
+
+const tags = [
+    {id:1, name:'Java'},
+    {id:2, name:'Spring Boot'},
+    {id:3, name:'JavaScript'},
+    {id:4, name:'ReactJs'},
+    {id:5, name:'React Native'},
+]
 
 function NavBar(){
+    const classes = useStyles();
+
     return (
-       <div>NavBar</div>
+       <Paper className={classes.root}>
+           <Button variant="outlined"color="secondary" className={classes.button}>Registra-se</Button>
+           <ListSubheader>
+               {'Tags em alta'}
+           </ListSubheader>
+           {
+               tags.map((item)=>(
+                    <ListItem dense button key={`item-${item.id}-${item.name}`}>
+                        <ListItemText primary={`#${item.name}`}/>
+                    </ListItem>
+               ))
+           }
+           <ListItem button>
+               Exibir mais
+           </ListItem>
+        </Paper>
     )
 }
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,9 @@ import Header from './components/Header'
 import NavBar from './components/NavBar'
 import Feed from './components/Feed'
 
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
@@ -13,21 +16,27 @@ const useStyles = makeStyles({
     },
     main:{
         height: '100vh',
-        width: '1200px',
-        display: 'flex',
-        margin: '0 auto'
+        padding: 24
+    },
+    toolbar:{
+        minHeight: 64
     }
 })
 
 function Home(){
     const classes = useStyles();
+    
     return(
         <div className={classes.root}>
             <Header />
-            <div className='toolbar'></div>
+            <div className={classes.toolbar}></div>
             <main className={classes.main}>
-                <NavBar />
-                <Feed />
+                <Container maxWidth="lg">
+                    <Box display="flex">
+                        <NavBar />
+                        <Feed />
+                    </Box>
+                </Container>
             </main>
         </div>
     )
