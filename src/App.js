88c35f35a669c2f5ff13f9a27f762744c2166bf7
Wleydson Lemos <wleydsonredes@gmail.com>
2021-01-05 19:37:43 -0300

    create theme custom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Home from './pages/home'
 
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import theme  from "./theme";
+
+
 function App() {
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/palette.js
@@ -0,0 +1,12 @@
+export default {
+  primary:{
+      dark: '#304ffe',
+      main: '#3d5afe',
+      light: '#536dfe'
+  },
+  secondary:{
+      dark: '#4caf50',
+      main: '#43a047',
+      light: '#388e3c'
+  }
+}
