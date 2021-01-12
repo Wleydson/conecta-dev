cc6c8900f4acd06420056c89e1b6830fe9554baf
Wleydson Lemos <wleydsonredes@gmail.com>
2021-01-12 18:58:44 -0300

    react router 6 beta

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Home from './pages/home'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import { ThemeProvider } from '@material-ui/core/styles';
 
 import theme  from "./theme";
+import SingIn from './pages/SingIn';
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/sing-in" element={<SingIn />}/>
+            <Route path="*" element={<Home />}/>
+          </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
---
+++ src/pages/SingIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SingIn() {
+    return(
+     <div>
+        Login Component
+     </div>   
+    )
+}
+
+export default SingIn
